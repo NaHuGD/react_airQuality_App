4761318908dbf113d714536aec5859982eecdd9b
asd2795asd <asd2795asd@gmail.com>
2020-07-23 10:45:29 +0800

    updata ghPages

---
+++ precache-manifest.0f7155e8a9a366e9b81603d21eca03e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe966d6eeea8d5716c5915f56fb9b0c5",
+    "url": "/react_airQuality_App/index.html"
+  },
+  {
+    "revision": "f5414b6e7d5f94303e89",
+    "url": "/react_airQuality_App/static/js/2.e1800b7f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react_airQuality_App/static/js/2.e1800b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e8b48d802f3a27ed60",
+    "url": "/react_airQuality_App/static/js/main.784211b0.chunk.js"
+  },
+  {
+    "revision": "3ff8f813b2abe121f55a",
+    "url": "/react_airQuality_App/static/js/runtime-main.06538aa5.js"
+  }
+]);
--- src/common/api.tsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-// 
-const airOpendata = axios.create({
-    baseURL: 'https://data.epa.gov.tw',
-})
-
-// 取得各地數據 API
-export const getAirData = (data = {}) => airOpendata.get('/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json', data)
